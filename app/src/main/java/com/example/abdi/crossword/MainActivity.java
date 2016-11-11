2bf5f5de9e394a7bef438ae0afebbbdd9ac8589d
Abdi <abdi1994@hotmail.com>
2016-11-11 20:28:08 +0000

    Initial commit

---
+++ app/src/main/java/com/example/abdi/crossword/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.abdi.crossword;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private void Test
+
+    {
+        int i = 1 + 1;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
